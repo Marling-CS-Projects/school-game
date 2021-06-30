50fb263c9d8f25857bcd64d60625a0cb52b01982
Joe Sharp <ratracejoe@hotmail.com>
2021-06-30 09:30:28 +0100

    simplify things a bit

--- src/GameObject2.ts
+++ src/GameObject2.ts
@@ -1,35 +1,34 @@
 import { Sprite } from 'pixi.js'; //importing libraries
-import { Body, Composite } from 'matter-js';
+import { Body } from 'matter-js';
 import * as uuid from 'uuid';
 
-export abstract class gameObject { //new class that defines a game object.
+export abstract class GameObject { //new class that defines a game object.
 
     pixiData: Sprite;  //type declarations for class properties Pixi = sprites only, 
     matterData: Body; // matter only accepts bodies
     uuid: string; //the indentifier for the object can only be a string.
-    
-        constructor(pixiData: Sprite, matterData: any) {
-        this.pixiData = pixiData; 
+
+    constructor(pixiData: Sprite, matterData: Body) {
+        this.pixiData = pixiData;
         this.matterData = matterData;
+
+        if (!this.matterData || !this.pixiData == false) { //checks that the data is actually there,
+            throw new Error('Invalid construction of game object')
+        }
 
         this.uuid = uuid.v4() //creates a unique identifier for the sprite.
     }
 
-    abstract spawn(): void; //creates a spawn property that is required to return void/
-
-    abstract gameloop(delta: number): void  
+    abstract gameloop(delta: number): void
 
     /**
      * updatePixiPosition, maps the pixi posistion data to to matter
      * 
      */
     public updatePixiPosition() {
-        if (!this.matterData || !this.pixiData == false){ //checks that the data is actually there,
-            return //errors out if it does not exist.
-        }
 
         this.pixiData.position.x = this.matterData.position.x; //mpaing pixi positioniny and rotation to matter. 
-        this.pixiData.position.y = this.matterData.position.y; 
+        this.pixiData.position.y = this.matterData.position.y;
         this.pixiData.rotation = this.matterData.angle
 
     }
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,60 @@
 import { Application, Loader, LoaderResource, PlaneGeometry, Rectangle, Sprite, Texture } from 'pixi.js';
-  import * as PIXI from "pixi.js";
-  import * as Matter from "matter-js"; 
-  import './style.css';
-  import { update } from 'lodash';
+import * as PIXI from "pixi.js";
+import * as Matter from "matter-js";
+import './style.css';
+import { update } from 'lodash';
 
-  let keys: any = {};
-  var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-    Body= Matter.Body;
-  let engine = Engine.create()
- 
-  const loader = PIXI.Loader
-  
-  
-  
-  function component() {
-    const element = document.createElement('div');
-  
-    //Creating the application/stage in PIXI
-    let app = new Application({ width: 800, height: 600}); 
-  
-    app.renderer.view.style.position = 'absolute';
-    app.renderer.view.style.display = "block";
-  
-    document.body.appendChild(app.view);
-  
-    //Introduces simple cube sprite from file. 
+let keys: any = {};
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies,
+  Body = Matter.Body;
+let engine = Engine.create()
 
-    
-    
-    //adds sprite to the application
-  
-    window.addEventListener("keydown", keysDown)
-    window.addEventListener("keyup", keysUp)
-  
-    function keysDown(e: any) {
-      console.log(e.keyCode)
-      keys[e.keyCode] = true;
-    }
-    
-    function keysUp(e: any) {
-      console.log(e.keyCode)
-      keys[e.keyCode] = false;
-    }
-    
-    function gameloop() {
-      //Right arrow
-      if (keys["40"]) {
-      }}
-      
-    
-    app.ticker.add(gameloop)
+const loader = PIXI.Loader
 
-    return element
+
+//Creating the application/stage in PIXI
+let app = new Application({ width: 800, height: 600 });
+
+app.renderer.view.style.position = 'absolute';
+app.renderer.view.style.display = "block";
+
+// Add the canvas to the document
+document.body.appendChild(app.view);
+
+//Introduces simple cube sprite from file. 
+
+
+
+//adds sprite to the application
+
+// Keeping track of which keys are pressed
+window.addEventListener("keydown", keysDown)
+window.addEventListener("keyup", keysUp)
+
+function keysDown(e: KeyboardEvent) {
+  console.log(e.code)
+  keys[e.code] = true;
+}
+
+function keysUp(e: KeyboardEvent) {
+  console.log(e.code)
+  keys[e.code] = false;
+}
+
+function gameloop() {
+  // Handle Directional Keys
+  if (keys["ArrowUp"]) {
   }
-  
-  //Key handlers
-  
-  
-  document.body.appendChild(component());
+  if (keys["ArrowDown"]) {
+  }
+  if (keys["ArrowLeft"]) {
+  }
+  if (keys["ArrowRight"]) {
+  }
+}
+
+
+app.ticker.add(gameloop)
